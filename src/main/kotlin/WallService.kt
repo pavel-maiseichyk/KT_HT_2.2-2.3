fdc9b47d30330e982667af163ecf52f8af7f2ab1
Pavel Maiseichyk <pavel.maiseichyk@gmail.com>
2020-12-25 20:27:48 +0300

    WallServiceCore.kt deleted

--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -1,7 +1,21 @@
-object WallService {
-    val service = WallServiceCore()
-}
+class WallServiceCore {
 
-fun main() {
+    private var posts = emptyArray<Post>()
 
+    fun add(post: Post): Post {
+        posts += if (posts.isEmpty()) post.copy(id = 1) else post.copy(id = posts.last().id + 1)
+        return posts.last()
+    }
+
+    fun update(post: Post): Boolean {
+        for ((index, currentPost) in posts.withIndex()) {
+            if (post.id == currentPost.id) {
+                posts[index] = post.copy(ownerId = currentPost.id, date = currentPost.id)
+                return true
+            }
+        }
+        return false
+    }
+
+    fun getLastPost():Int = if (posts.isEmpty()) 0 else posts.last().id
 }
